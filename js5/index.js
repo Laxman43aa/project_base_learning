5c191a2f7aeb4405e65c5602d260f8b5214f0d6c
Laxman Rawat <152087699+Laxman43aa@users.noreply.github.com>
2025-06-24 21:40:44 +0530

    create a faulty calculater
    using js create a faulty calculater.

---
+++ js5/index.js
@@ -0,0 +1,39 @@
+/* Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+
+It performs wrong operation 10% of the times
+
+*/
+
+
+let random = Math.random();
+ let a = prompt("Enter first number: ");
+ let c = prompt("Enter operation: ");
+ let b = prompt("Enter second number: ");
+
+ let obj ={
+    "+": "-",
+    "-": "/",
+    "*": "+",
+    "/": "**"
+ };
+
+ if(random < 0.1){
+    //perfom correct calculation
+    console.log(`the result is ${a} ${c} ${b} `);
+    alert(`the result is ${eval(`${a} ${c} ${b}`)}`);
+ }
+    else{
+        //perform faulty calculation
+        c = obj[c];
+        alert(`the result is ${eval(`${a} ${c} ${b}`)}`);
+ }
