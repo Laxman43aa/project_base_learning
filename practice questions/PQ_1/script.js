08ea1ed624c569bf2278158008aada6ddfde6d2c
Laxman Rawat <152087699+Laxman43aa@users.noreply.github.com>
2025-06-28 19:08:10 +0530

    js practice questions .
    js practice questions.

---
+++ practice questions/PQ_1/script.js
@@ -0,0 +1,19 @@
+   const product = {
+    title:"ballpan",
+    rating: 4,
+    price:270,
+    offer: "5%"
+};    
+
+console.log(product);
+
+const profile = {
+    name : "shradha khapra",
+    posts: 195,
+    followers: "590K",
+    following : 4,
+    isFollow:true,
+    details:" Apna college"
+}
+
+console.log(profile);
