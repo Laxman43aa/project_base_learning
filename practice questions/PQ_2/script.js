77985d3a7f626be4a1bb9fffd6a0a3e04c70ef54
Laxman Rawat <152087699+Laxman43aa@users.noreply.github.com>
2025-06-29 18:07:33 +0530

    practice
    practice js.

---
+++ practice questions/PQ_2/script.js
@@ -0,0 +1,23 @@
+// let number = prompt("enter a number")
+// if(number % 5 === 0){
+//     console.log(number , " is multiple of 5");
+// }
+// else{
+//     console.log(number , " is not multiple of 5");
+// }
+
+let marks = prompt("Enter your marks");
+if (marks >= 90 && marks <= 100) {
+    console.log(" according to your marks your Grade is A");
+}else if (marks >= 70 && marks <= 89) {
+    console.log(" according to your marks your Grade is B"); 
+}
+else if (marks >= 60 && marks <= 69) {
+    console.log(" according to your marks your Grade is C");
+}
+else if (marks >= 50 && marks <= 59) {
+    console.log(" according to your marks your Grade is D");
+
+}else if (marks >= 0 && marks <= 49) {
+    console.log(" according to your marks your Grade is F");
+}
