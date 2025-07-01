75bbc129fecbe31cb4f8933006fddc76c327ffdb
Laxman Rawat <152087699+Laxman43aa@users.noreply.github.com>
2025-07-01 20:42:29 +0530

    js loop practice
    loop practice js

---
+++ practice questions/PQ_3/script.js
@@ -0,0 +1,19 @@
+// print all eveven numbers from 0 to 100
+// for(let i = 0; i<= 100; i++){
+//     if(i%2===0){
+//         console.log("even number: " , i);
+//     }
+    // }else{
+    //     // console.log("Odd number: " + i);
+    // }
+
+
+// qu_2
+
+let gameNum = 25;
+let userNum = prompt("gess the game number");
+while(userNum != gameNum){
+   userNUM = prompt("wrong guess, gess again ");
+
+}
+console.log("congratulations, you guessed the number correctly");
