2fa3f948ab70a7606ac1df3165c0196bacc31501
Laxman Rawat <152087699+Laxman43aa@users.noreply.github.com>
2025-07-12 22:21:34 +0530

    Update script.js
    add some new consepts promise chain, async- await, and IIFE.

--- practice questions/PQ_12/script.js
+++ practice questions/PQ_12/script.js
@@ -95,3 +95,109 @@ promise.catch((err)=>{
 //     },5000);
 // })
 // }
+// promise work 
+
+// function asyncFunc1(){
+//     return new Promise((resolve , reject)=>{
+//         setTimeout(()=>{
+//             console.log("data1");
+//             resolve("success");
+//         },4000)
+//     })
+// }
+// function asyncFunc2(){
+//     return new Promise((resolve , reject)=>{
+//         setTimeout(()=>{
+//             console.log("data2");
+//             resolve("success");
+//         },4000)
+//     })
+// }
+// //  promise chain work..
+// console.log("fatch data1 .....");
+//  asyncFunc1().then((res)=>{
+//     console.log("fatch data2 ....");
+//      asyncFunc2().then((res)=>{
+//     })
+// }
+// )
+// second example of promise chain work
+// function getData(dataId) {
+//     return new Promise((resolve, reject) => {
+//         // 2s
+//         setTimeout(() => {
+//             console.log("data", dataId);
+//             resolve("success");
+//         }, 3000);
+//     })
+// }
+// promise chain example 
+// getData(1).then((res) => {
+//     console.log(res);
+//     getData(2).then((res) => {
+//         console.log(res);
+//     })
+
+// })
+
+// or 
+
+// promise chain 
+// console.log("getting data1 ...");
+// getData(1)
+// .then((res) => {
+//     console.log("getting data2 ...");
+//     return getData(2);
+// }).then((res)=>{
+//     console.log("getting data3 ...");
+//     return getData(3);
+// }).then((res)=>{
+//     console.log(res)
+// })
+
+// async await
+// async function hello(){
+//     console.log("hello");
+// }
+
+// function api(){
+//     return new Promise((resolve , reject)=>{
+//         setTimeout(()=>{
+//             console.log("weather data");
+//             resolve(200);
+//         },2000);
+//     })
+// }
+
+// async function getWeatherData(){
+//     await api(); //1st call
+//     await api(); //2nd call
+// }
+
+function getData(dataId) {
+    return new Promise((resolve, reject) => {
+        // 2s
+        setTimeout(() => {
+            console.log("data", dataId);
+            resolve("success");
+        }, 2000);
+    })
+}
+// Async-await 
+async function getAllData(){
+    console.log("getting data1 ...");
+    await getData(1);
+    console.log("getting data2 ...");
+    await getData(2);
+    console.log("getting data3 ...");
+    await getData(3);
+}
+// IIFE : Immedietly invoked function expresion (func)();
+(async function getAllData(){
+    console.log("getting data1 ...");
+    await getData(1);
+    console.log("getting data2 ...");
+    await getData(2);
+    console.log("getting data3 ...");
+    await getData(3);
+})();
