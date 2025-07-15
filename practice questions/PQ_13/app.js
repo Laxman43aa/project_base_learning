932ba49b1f7fc9b7239073178a7e4211ddb98cc0
Laxman Rawat <152087699+Laxman43aa@users.noreply.github.com>
2025-07-15 20:07:41 +0530

    Update app.js
    api calls and working of fetch api and fetch method

--- practice questions/PQ_13/app.js
+++ practice questions/PQ_13/app.js
@@ -1,10 +1,26 @@
-const URL ="https://official-joke-api.appspot.com/random_joke";
+// API calls
+// using fetch api get random facts 
+const URL ="https://uselessfacts.jsph.pl/random.json";
+const factPara = document.querySelector("#fact");
+const btn  = document.querySelector("#btn");
 
 const getFacts =  async () =>{
     console.log("getting data ......");
     let response = await fetch(URL);
-console.log(response);
-let data= await response.json();
-console.log(data);
-
+console.log(response);// not readable on json format
+let data= await response.json(); //then we can call .json for converting readable formate
+// console.log(data.text);
+factPara.innerText = data.text ;
 };
+
+// using promise chain
+// function getFacts(){
+//     fetch(URL).then((response)=>{
+//         return response.json();
+//     }).then((data)=>{
+//         console.log(data);
+//         factPara.innerText = data.text ;
+// });
+
+// }
+btn.addEventListener("click", getFacts);
